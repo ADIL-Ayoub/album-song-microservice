ab2732fe83e5212b9e74ae8d27a70538ea7d9cc5
ADIL Ayoub <samih287@gmail.com>
2023-11-18 12:15:29 +0100

    working without a git repo

--- album-service/src/main/java/com/learning/albumservice/client/SongClient.java
+++ album-service/src/main/java/com/learning/albumservice/client/SongClient.java
@@ -1,15 +1,21 @@
 package com.learning.albumservice.client;
 
 import com.learning.albumservice.model.Song;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "song-service", url = "${application.config.song-url}")
+@FeignClient(name = "SONG-SERVICE")
 public interface SongClient {
-
-    @GetMapping("/album/{albumId}")
+    @GetMapping("/api/v1/songs/album/{albumId}")
+    @CircuitBreaker(name="song-client",fallbackMethod = "defaultgetAllSongsByAlbumId")
     public List<Song> getAllSongsByAlbumId(@PathVariable("albumId") Integer albumId);
+
+    default List<Song> defaultgetAllSongsByAlbumId(Exception exception){
+        return List.of();
+    }
+
 }
--- gateway-api/src/main/java/com/learning/gatewayapi/GatewayApiApplication.java
+++ gateway-api/src/main/java/com/learning/gatewayapi/GatewayApiApplication.java
@@ -3,6 +3,10 @@ package com.learning.gatewayapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -12,4 +16,11 @@ public class GatewayApiApplication {
 		SpringApplication.run(GatewayApiApplication.class, args);
 	}
 
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc,
+												  DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+	}
 }
+
